bb68dc9fa1ccecaaed7dd4c7403c7d1f3a8ea563
Enemby <Enemby@users.noreply.github.com>
2020-04-18 04:04:36 -0600

    Added Walk Animation

--- DyingIsGood/Assets/My Assets/Scripts/Player.js
+++ DyingIsGood/Assets/My Assets/Scripts/Player.js
@@ -2,9 +2,14 @@
 var stopFriction : float = 0.8;
 var jumpSpeed : float = 5;
 var groundDistance : float = 0.5;
+var walkingSprites : Sprite[];
 var aliveSprite : Sprite;
 var deadSprite : Sprite;
 var deadClip : AudioClip;
+private var grounded : boolean = false;
+private var privTimer : float = 0;
+private var spriteIndex : int = 0;
+private var newFrame : boolean = false;
 function Movement(){
 	this.GetComponent(Rigidbody2D).velocity = Vector2(this.GetComponent(Rigidbody2D).velocity.x+Input.GetAxis("Horizontal")*moveSpeed*4,this.GetComponent(Rigidbody2D).velocity.y);
 }
@@ -16,10 +21,12 @@ function groundCheck(){
 	if(hit.collider != null){
 		return true;
 		Debug.Log("True!");
+		grounded = true;
 	}
 	else{
 		return false;
 		Debug.Log("False!");
+		grounded = false;
 	}
 }
 function Jump(){
@@ -47,11 +54,44 @@ function Die(){
 	this.gameObject.tag = "Untagged";
 	this.enabled = false;
 }
+function iterateWalkSprites(){
+	if(newFrame == true){
+		spriteIndex += 1;
+		newFrame = false;
+	}
+	if(spriteIndex > walkingSprites.Length-1){
+		spriteIndex = 0;
+	}
+	this.GetComponent(SpriteRenderer).sprite = walkingSprites[spriteIndex];
+	if(Input.GetAxis("Horizontal") > 0){
+		this.transform.localScale.x = 1;
+	}
+	if(Input.GetAxis("Horizontal") < 0){
+		this.transform.localScale.x = -1;
+	}
+}
+function resetSprite(){
+	this.GetComponent(SpriteRenderer).sprite = aliveSprite;
+}
+function spriteTimer(){
+	privTimer+=Time.deltaTime;
+	if(privTimer > 0.3){
+		privTimer = 0;
+		newFrame = true;
+	}
+}
 function Update(){
 	Movement();
+	if(Input.GetAxis("Horizontal") != 0 && groundCheck() == true){
+		iterateWalkSprites();
+	}
+	else{
+		resetSprite();
+	}
 	if(Input.GetButton("Jump") && groundCheck() == true){
 		Jump();
 	}
+	spriteTimer();
 }
 function FixedUpdate(){
 	Friction(); //So it's not framerate dependant
